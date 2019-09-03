b1d442cd4d2e425c83db91ada1d7653379d1da57
kevin <picadokevin3@gmail.com>
2019-09-02 22:50:11 -0600

    Listo getAllproducts,tambien el qu identifica el key

--- Proyecto.Model/Data/Conexion.cs
+++ Proyecto.Model/Data/Conexion.cs
@@ -1,17 +1,29 @@
-﻿using Npgsql;
+﻿
+using MySql.Data.MySqlClient;
+using Npgsql;
+using Proyecto.Model.Domain;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+
 
 namespace Proyecto.Model.Data
 {
-   public class Conexion
+    public class Conexion
     {
+
+        String connectionString;
+
+        public Conexion(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
         public NpgsqlConnection connection()
         {
             NpgsqlConnection connection = new NpgsqlConnection();
             var connectionChain = "server=;Port=;UserId=;Password=;Database=";
-            if(!string.IsNullOrWhiteSpace(connectionChain))
+            if (!string.IsNullOrWhiteSpace(connectionChain))
             {
                 try
                 {
@@ -19,13 +31,70 @@ namespace Proyecto.Model.Data
                     connection.Open();
 
                 }
-                catch(Exception)
+                catch (Exception)
                 {
                     connection.Close();
                 }
             }
             return connection;
         }
-        
+
+
+        public List<Key> GetKey(String Key)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres; " +
+                 "Password=pwd;Database=postgres;");
+            conn.Open();
+
+            // Define a query returning a single row result set
+            NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM cities", conn);
+
+            NpgsqlDataReader rdr = command.ExecuteReader();
+            List<Key> keylist = new List<Key>();
+            while (rdr.Read())
+            {
+                Key key = new Key();
+
+
+                key.CodKey = rdr["Name"].ToString();
+                key.Status1 = Convert.ToInt32(rdr["StudentID"]);
+
+                keylist.Add(key);
+            }
+            conn.Close();
+            return keylist;
+        }
+
+
+
+        public List<Producto> getAllProducts(String key)
+        {
+
+            MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;User Id=postgres; " +
+                     "Password=pwd;Database=postgres;");
+            conn.Open();
+
+            // Define a query returning a single row result set
+            MySqlCommand command = new MySqlCommand("SELECT ALL FROM productos", conn);
+
+            MySqlDataReader rdr = command.ExecuteReader();
+            List<Producto> productList = new List<Producto>();
+            if (GetKey(key).Count>=1)
+                while (rdr.Read())
+                {
+                    Producto product = new Producto();
+
+                    product.CodProduct = Convert.ToInt32(rdr["StudentID"]);
+                    product.NameProduct = rdr["Name"].ToString();
+                    product.PriceProduct = Convert.ToInt32(rdr["StudentID"]);
+                    product.DescriptionProduct = rdr["Name"].ToString();
+                    product.Image = rdr["Name"].ToString();
+                    product.Category.CodCategory = Convert.ToInt32(rdr["StudentID"]);
+                    
+                   productList.Add(product);
+                }
+            conn.Close();
+            return productList;
+        }
     }
 }
---
+++ Proyecto.Model/Domain/Key.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto.Model.Domain
+{
+    public class Key
+    {
+        private String codKey;
+        private int Status;
+
+        public Key()
+        {
+        }
+
+        public Key(string codKey, int status)
+        {
+            this.codKey = codKey;
+            Status = status;
+        }
+
+        public string CodKey { get => codKey; set => codKey = value; }
+        public int Status1 { get => Status; set => Status = value; }
+    }
+}
